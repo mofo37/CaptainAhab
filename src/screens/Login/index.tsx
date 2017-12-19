12b0f3301c2a571ab6aacfc5faf36fce19397832
Chris_Wallace <kawallis@icloud.com>
2017-12-19 09:25:56 -0800

    changed back login

--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -1,8 +1,9 @@
 import * as React from "react";
-import { Platform, ImageBackground } from "react-native";
+import { Platform } from "react-native";
 
-import {  Body, Title, Button, Text, View, Icon } from "native-base";
-// import styles from "./styles";
+import { Container, Content, Body, Title, Button, Text, View, Icon } from "native-base";
+// let { height, width } = Dimensions.get('window')
+import styles from "./styles";
 export interface Props {
 	loginForm: any;
 	onLogin: Function;
@@ -10,20 +11,38 @@ export interface Props {
 	screenProps: any;
 	onGoogleLogin: any;
 }
-export interface State {}
+export interface State { }
 class Login extends React.Component<Props, State> {
-	render() {		
+	render() {
 		return (
-			// <Container style={styles.container}>
-			// 	<Content >
-				<ImageBackground
-					source={{ uri: 'https://images.unsplash.com/photo-1504133240992-d676c40f1d93?auto=format&fit=crop&w=750&q=80' }}
-				>
+			<Container style={styles.container}>
+				<Content >
 					{/* <Icon style={{marginLeft: 15}} name='ios-close-circle-outline' onPress={() => this.props.screenProps.dismiss() }/> */}
-					{/* <Body style={{ alignItems: "center", marginTop: 50 }}> */}
-					
-			</ImageBackground>
-				
+					<Body style={{ alignItems: "center", marginTop: 50 }}>
+						<Icon name="flash" style={{ fontSize: 104 }} />
+						<Title>ReactNativeSeed.com</Title>
+						<View padder>
+							<Text style={{ color: Platform.OS === "ios" ? "#000" : "#FFF" }} />
+						</View>
+					</Body>
+					{this.props.loginForm}
+					<View padder style={{ marginTop: 10 }}>
+						<Button block onPress={() => this.props.onLogin()}>
+							<Text>Login</Text>
+						</Button>
+					</View>
+					<View padder>
+						<Button style={{ backgroundColor: '#FF5722' }} block onPress={() => this.props.onGoogleLogin()}>
+							<Icon name="logo-google" />
+							<Text>Login with Google</Text>
+						</Button>
+					</View>
+					<View padder style={{ marginTop: 10 }}>
+						<Text style={{ textAlign: 'center', textDecorationLine: 'underline' }} onPress={() => this.props.navigation.navigate('Register')}>Don't Have Account? Sign Up</Text>
+					</View>
+				</Content>
+			</Container>
+
 		);
 	}
 }
