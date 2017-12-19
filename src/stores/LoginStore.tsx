f20268f5c859eaa0d12835fd22e825a0e40884e2
Morgan Fogarty <mofo37@gmail.com>
2017-12-18 23:15:02 -1000

    refactor validateEmail()

--- src/stores/LoginStore.tsx
+++ src/stores/LoginStore.tsx
@@ -10,25 +10,30 @@ const Login = types
 		isLoggedIn: types.optional(types.boolean, false),
 	})
 	.actions(self => {
-		function loggedInChange(event) {
-			self.isLoggedIn = event;
+		function loggedInChange(loggedInState) {
+			self.isLoggedIn = loggedInState;
 		}
 		function emailOnChange(id) {
 			self.email = id;
 			validateEmail();
 		}
+		
 		function validateEmail() {
-			const emailPatter = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
-			const required = self.email ? undefined : "Required";
-			self.emailError = required ? required : emailPatter.test(self.email) ? undefined : "Invalid email address";
+			const emailPattern = /.+@.+\..+/;
+			let emailError = undefined;
+			if (!self.email || !emailPattern.test(self.email)) {
+				emailError = 'Invalid';
+			}  
+			self.emailError = emailError;
 		}
+
 		function passwordOnChange(pwd) {
 			self.password = pwd;
 			validatePassword();
 		}
 		function validatePassword() {
 			const alphaNumeric = /[^a-zA-Z0-9 ]/i.test(self.password) ? "Only alphanumeric characters" : undefined;
-			const maxLength = self.password.length > 15 ? "Must be 15 characters or less" : undefined;
+			const maxLength = self.password.length > 15 ? "Must be 15 characters or fewer" : undefined;
 			const minLength = self.password.length < 8 ? "Must be 8 characters or more" : undefined;
 			const required = self.password ? undefined : "Required";
 			self.passwordError = required ? required : alphaNumeric ? alphaNumeric : maxLength ? maxLength : minLength;
